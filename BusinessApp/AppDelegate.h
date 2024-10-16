668d9b2c642cec621316aa39c5120e8b8893cd47
Alex Denusiuk <alexdenusyk@gmail.com>
2024-10-16 22:04:24 +0300

    Initial Commit

---
+++ BusinessApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BusinessApp
+//
+//  Created by Dev on 16.10.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BusinessApp/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BusinessApp
+//
+//  Created by Dev on 16.10.2024.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
